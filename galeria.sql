78a5e76abb44e5c7cd8264294e1b59f73a9d5b8b
rPlakama <rPlakama@proton.me>
2025-11-10 19:10:10 -0300

    Remoção de comentários

--- galeria.sql
+++ galeria.sql
@@ -1,4 +1,3 @@
-/* DADOS DOS ARTISTAS */
 
 CREATE TABLE IF NOT EXISTS artistas(
 id INT auto_increment primary key,
@@ -9,10 +8,8 @@ Nacionalidade varchar(255) not null
 
 replace into artistas (artista, PeriodoAtuacao, Nacionalidade)
 values  ('Vincent van Gogh', '1853-1890', 'Neerlandês'),
-	('Pablo Picasso', '1881-1973', 'Espanhol'),
-        ('AnitaMalfatti', '1889-1964', 'Brasileira');
-
-/* DADOS DA PROGRAMAÇÃO GALERIA */
+      ('Pablo Picasso', '1881-1973', 'Espanhol'),
+      ('AnitaMalfatti', '1889-1964', 'Brasileira');
 
 CREATE TABLE IF NOT EXISTS galeria(
   id INT AUTO_INCREMENT PRIMARY KEY,
@@ -23,10 +20,9 @@ CREATE TABLE IF NOT EXISTS galeria(
 
 replace  into galeria (excursoes, horarios, Dias)
 values  ('Vincent van Gogh', '09:00-12:00', 'Sexta-Feira'),
-	('Pablo Picasso', '14:00-16:00', 'Sábado'),
-        ('Anita Malfatti', '17:00-19:00', 'Domingo');
+      ('Pablo Picasso', '14:00-16:00', 'Sábado'),
+      ('Anita Malfatti', '17:00-19:00', 'Domingo');
   
-/* DADOS DA PROGRAMAÇÃO FUNCIONARIOS */
 
 CREATE TABLE IF NOT EXISTS funcionarios(
   id INT AUTO_INCREMENT PRIMARY KEY,
@@ -37,11 +33,10 @@ CREATE TABLE IF NOT EXISTS funcionarios(
 
 replace  into funcionarios (nome, horarios, especialidade)
 values  ('Ana Carolina', '09:00 às 10:30', 'Vicent Van Gogh'),
-	('Bruno Mendes', '10:30 às 12:00', 'Vicent Van Gogh'),
-        ('Carla Dias', '14:00 às 16:00', 'Pablo Picasso'),
-        ('David Silva', '17:00 às 19:00', 'Anita Malfatti');
+      ('Bruno Mendes', '10:30 às 12:00', 'Vicent Van Gogh'),
+      ('Carla Dias', '14:00 às 16:00', 'Pablo Picasso'),
+      ('David Silva', '17:00 às 19:00', 'Anita Malfatti');
 
-/* OBRAs DE VAN GOGH */
 
 CREATE TABLE IF NOT EXISTS VanGogh(
 id INT auto_increment primary key,
@@ -51,12 +46,10 @@ ObraData varchar(255) not null
 
 replace  into VanGogh (Arte, ObraData)
 values  ('A Noite Estrelada', '1889'),
-	('Auto-Retrato', '1889'),
-        ('O Quarto de Van Gogh em Arles', '1888'),
-        ('Noite Estrelada Sobre o Ródano', '1888'),
-        ('A Vinha Encarnada', '1888');
-        
-/* OBRAs DE PabloPicasso */
+      ('Auto-Retrato', '1889'),
+      ('O Quarto de Van Gogh em Arles', '1888'),
+      ('Noite Estrelada Sobre o Ródano', '1888'),
+      ('A Vinha Encarnada', '1888');
         
 CREATE TABLE IF NOT EXISTS PabloPicasso(
 id INT auto_increment primary key,
@@ -66,12 +59,11 @@ ObraData varchar(255) not null
 
 replace  into PabloPicasso (Arte, ObraData)
 values  ('Guernica', '1937'),
-	('Vieux guitariste aveugle', '1903'),
-        ('Mulher no Espelho', '1932'),
-        ('Le Rêve', '1932'),
-        ('Retrato de Dora Maar', '1937');
+      ('Vieux guitariste aveugle', '1903'),
+      ('Mulher no Espelho', '1932'),
+      ('Le Rêve', '1932'),
+      ('Retrato de Dora Maar', '1937');
         
-/* OBRAs de AnitaMalfatti */
         
 CREATE TABLE IF NOT EXISTS AnitaMalfatti(
 id INT auto_increment primary key,
@@ -81,10 +73,10 @@ ObraData varchar(255) not null
 
 replace  into AnitaMalfatti (Arte, ObraData)
 values  ('O homem amarelo', '1917'),
-	('Mario de Andrade I', '1922'),
-        ('Samba', '1945'),
-        ('A Boba', '1916'),
-        ('A Onda', '1917');
+      ('Mario de Andrade I', '1922'),
+      ('Samba', '1945'),
+      ('A Boba', '1916'),
+      ('A Onda', '1917');
 
 
 
